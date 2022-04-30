f9d2d13e70decbaee93abf69fe7927bf41977bb6
Dishant Sharma <98343608+dishantsharma7@users.noreply.github.com>
2022-04-30 23:20:23 +0530

    Time: 4 ms (42.57%), Space: 8.6 MB (85.85%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    { 
+        for (int i=digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+                digits[i]+=1;
+                break;
+            }
+            else
+            {
+                digits[i]=0;
+                // check if this is the first digit in array
+                //for example [9] will turn into [1,0] as required
+                if (i == 0)
+                {
+                    digits.insert(digits.begin(), 1);
+                    break;
+                }   
+            } 
+        }
+        return digits;
+    }
+};
