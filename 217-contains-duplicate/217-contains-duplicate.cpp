09ee022db8507480329978fe9de8d1147e8c4d42
Dishant Sharma <98343608+dishantsharma7@users.noreply.github.com>
2022-08-09 12:00:40 +0530

    Time: 199 ms (15.32%), Space: 52.1 MB (38.23%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> countmap;
+        for(int i=0;i<nums.size();i++){
+            if(countmap[nums[i]]){
+                return true;
+            }countmap[nums[i]]++;
+        }
+        return false;
+    }
+       
+};
