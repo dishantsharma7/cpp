83f166bd144aaf29bda542932b9a545c4394bee3
Dishant Sharma <98343608+dishantsharma7@users.noreply.github.com>
2022-09-14 23:45:06 +0530

    Time: 35 ms (9.77%), Space: 6 MB (25.30%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,29 +1,12 @@
-// class Solution {
-// public:
-//     int mySqrt(int x) {
-//         long long num=1;
-//         if(x==0||x==1)
-//             return x;
-//         while(num*num<=x){
-//             num++;
-//         }
-//         return num-1;
-//     }
-// };
-class Solution{
-    public:
-    int mySqrt(int x){
-       long long i,num;
-        if(x<=1)
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long num=1;
+        if(x==0||x==1)
             return x;
-        for(i=1;i<=x;i++)
-        {
-            if(i*i>x)
-            {
-                num=i-1;
-                break;
-            }
+        while(num*num<=x){
+            num++;
         }
-        return num;
+        return num-1;
     }
 };
